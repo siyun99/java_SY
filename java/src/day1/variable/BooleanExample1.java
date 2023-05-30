dafed7d4147661874ebd2516c457393fe0d86c81
siyun99 <134987728+siyun99@users.noreply.github.com>
2023-05-30 15:08:07 +0900

    자바 : 변수 기초

---
+++ java/src/day1/variable/BooleanExample1.java
@@ -0,0 +1,10 @@
+package day1.variable;
+
+public class BooleanExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/src/day1/variable/FloatExample.java
@@ -0,0 +1,10 @@
+package day1.variable;
+
+public class FloatExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
