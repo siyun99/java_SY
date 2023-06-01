0615e5d97c685007372ff2d2916f44869bf129e4
siyun99 <134987728+siyun99@users.noreply.github.com>
2023-06-01 13:37:29 +0900

    산술연산자에서 정수 타입 예제

---
+++ java/src/day3/operator/Ex1.java
@@ -0,0 +1,17 @@
+package day3.operator;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		int score = 65;
+		String str;
+		str = score >= 60 ? "true" : "false";
+		System.out.println("Do you pass 65 points?"+str);
+		
+		int score1 = 45;
+		String str1;
+		str = score >= 60 ? "true" : "false";
+		System.out.println("Do you pass 65 points?"+str);
+	}
+
+}
