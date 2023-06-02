051e1a7716955784099744c252cba83272926f3d
siyun99 <134987728+siyun99@users.noreply.github.com>
2023-06-02 14:47:57 +0900

    자바:if else문 - 2의 배수 예제

---
+++ java/src/day4/practice/IfMultipleEx1.java
@@ -0,0 +1,33 @@
+package day4.practice;
+
+import java.util.Scanner;
+
+public class IfMultipleEx1 {
+
+		public static void main(String[] args) {
+
+		int num;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("정수가 2의 배수인가?");
+
+		num = sc.nextInt();
+
+
+
+		if((num % 2) == 0) {
+
+			System.out.println(num + "는(은) 2의 배수입니다");
+
+		} else {
+
+			System.out.println(num + "는(은) 2의 배수가 아닙니다");
+
+		}
+
+	}
+
+
+
+}
