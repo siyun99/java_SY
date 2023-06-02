f3822633011677eb675ed5bf74d4f7fb09fb2591
siyun99 <134987728+siyun99@users.noreply.github.com>
2023-06-02 14:47:14 +0900

    3일차 숙제

---
+++ java/src/day3/homework/Ex1_Conditional2.java
@@ -0,0 +1,27 @@
+package day3.homework;
+
+import java.util.Scanner;
+
+public class Ex1_Conditional2 {
+
+	public static void main(String[] args) {
+		/*성별(M:남성,W:여성)를 입력받아 M이면 남성을,W이면 여성을 출력하는 코드를 작성하세요.
+		 * input gender;
+		 * M
+		 * Are you a woman? false
+		 * W
+		 * Are you a woman?true
+		 */
+		
+		char gender;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input gender.");
+		gender=sc.next().charAt(0);
+		boolean result = gender != 'W' ? true : false;
+		System.out.println("Are you a woman?" + result);
+		
+		sc.close();
+		
+	}
+
+}
