90fc2b57a97b158b5beff78e1213bbc28f1a3a7c
siyun99 <134987728+siyun99@users.noreply.github.com>
2023-06-02 14:39:20 +0900

    3일차 숙제

---
+++ java/src/day4/ifex/NumberEx.java
@@ -0,0 +1,27 @@
+package day4.ifex;
+
+public class NumberEx {
+
+	public static void main(String[] args) {
+		/* 정수 num가 양수이면 positive number라고 출력하고 
+		 * num가 음수이면 negative number로 출력하고
+		 * num가 0 이면 0으로 출력하는 코드를 작성하세요.
+		 */
+		int num = -1;
+		//정수 num가 양수이면 positive number라고 출력하고 
+		if(num>0) {
+			System.out.println(num + " is a positive number.");
+		}else if(num<0) {
+			System.out.println(num + " is a negative number.");
+		}else {
+			System.out.println(0);
+		}
+		
+		
+			
+		
+		
+		
+	}
+
+}
