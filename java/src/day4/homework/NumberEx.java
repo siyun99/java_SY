270b98ad1a72c2b1f9ff5fe839d74901ae907165
siyun99 <134987728+siyun99@users.noreply.github.com>
2023-06-05 10:12:55 +0900

    자바 : 결합 대입 연산자

---
+++ java/src/day4/homework/NumberEx.java
@@ -0,0 +1,27 @@
+package day4.homework;
+
+import java.util.Scanner;
+
+public class NumberEx {
+
+	public static void main(String[] args) {
+		/* 정수 num가 짝수이면 num에 2를 나누고,
+		 * 정수 num가 홀수이면 num에 1을 더한 후 , num를 출력하는 코드를 작성하세요.
+		 */
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input integer number : ");
+		num = sc.nextInt();
+		//정수 입력
+		
+		if(num % 2 ==0) {
+			num = num/2;
+			//num /=2;
+		}else {
+			num += 1;
+			//num = num +1;
+		}
+		System.out.println(num);
+	}
+
+}
