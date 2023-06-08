93c87b6aa3ea33cac62749979e123948b55a7974
siyun99 <134987728+siyun99@users.noreply.github.com>
2023-06-08 14:43:56 +0900

    자바 : 반복문 알파벳 출력 예제

---
+++ java/src/day7/practice/PrintAlphabetEx2.java
@@ -0,0 +1,35 @@
+package day7.practice;
+
+public class PrintAlphabetEx2 {
+
+	public static void main(String[] args) {
+		/* 다음과 같이 출력되는 코드를 작성하세요.
+		 * a
+		 * ab
+		 * abc
+		 * ...
+		 * abcde...z
+		 */
+		
+		/* 외부 반복문
+		 * 반복횟수 : end는 'a'부터 시작해서 'z'까지 1씩 증가
+		 * 규칙성 : 'a'부터 end까지 출력후 엔터
+		 * 반복문 종료 후 : 없음 
+		 */
+		
+			/* 내부 반복문
+			 * 반복횟수 : ch는 'a'부터 시작해서 end까지 1씩 증가
+			 * 규칙성 : ch를 출력
+			 * 반복문 종료 후: 없음
+			 */
+		char ch = 'a' ,end;
+		for(end='a';end<='z';end++) {
+			for(ch='a';ch<=end;ch++) {
+				System.out.print(ch);
+		}
+		System.out.println();
+			
+		}
+	}
+
+}
