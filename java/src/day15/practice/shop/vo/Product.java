e9e28cf64aa01e814520d3b4ea9d04e562439abb
siyun99 <134987728+siyun99@users.noreply.github.com>
2023-06-21 14:48:32 +0900

    자바 : 제품 판매 프로그램 예제 - 구현 완료

--- java/src/day15/practice/shop/vo/Product.java
+++ java/src/day15/practice/shop/vo/Product.java
@@ -20,6 +20,14 @@ public class Product {
 		this.category = category;
 	}
 	
+	public Product(Product product) {
+		this.name = product.name;
+		this.modelName = product.modelName;
+		this.price = product.price;
+		this.amount = product.amount;
+		this.category = product.category;
+	}
+
 	//메서드
 	/**제품 입고 기능 => 현재 수량에 주어진 수량을 누적*/
 	public void store(int amount) {
@@ -38,5 +46,13 @@ public class Product {
 	}
 	private void accumulate(int amount) {
 		this.amount += amount;
+	}
+
+	public void print() {
+		System.out.println("제품명 : " + name);
+		System.out.println("모델명 : " + modelName);
+		System.out.println("분류  : " + category);
+		System.out.println("제고  : " + amount);
+		System.out.println("가격  : " + price);
 	}
 }
