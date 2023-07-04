b6f15cfad69d770d6c7129651a609d51cbea8047
siyun99 <134987728+siyun99@users.noreply.github.com>
2023-07-04 14:27:24 +0900

    자바 : 대학생 정보 관리 프로그램 예제

---
+++ java/src/day25/student/run/Run.java
@@ -0,0 +1,12 @@
+package day25.student.run;
+
+import day25.student.controller.StudentController2;
+
+public class Run {
+
+	public static void main(String[] args) {
+		StudentController2 sc = new StudentController2();
+		sc.run();
+	}
+
+}
