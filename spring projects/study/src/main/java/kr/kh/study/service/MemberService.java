3d02d3f913f3c1e47a013c560d69c51a1727cc48
siyun99 <134987728+siyun99@users.noreply.github.com>
2023-08-31 17:30:50 +0900

    SPRING 복습 : 로그인 기능 구현 완료

--- spring projects/study/src/main/java/kr/kh/study/service/MemberService.java
+++ spring projects/study/src/main/java/kr/kh/study/service/MemberService.java
@@ -1,7 +1,12 @@
 package kr.kh.study.service;
 
+import kr.kh.study.vo.MemberVO;
+
 public interface MemberService {
 
-	int count();
+	boolean signup(MemberVO member);
 
+	MemberVO login(MemberVO member);
+
+	
 }
